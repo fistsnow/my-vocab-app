670835edcaf2ffea8c90d964472e364d50314010
이명희 <imyeonghui@imyeonghuis-MacBook-Air.local>
2025-07-09 23:41:56 +0900

    Remove sudo from build script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# ⭐️ 관리자 권한으로 실행하기 위해 sudo 추가
-sudo apt-get clean
-sudo apt-get update
-sudo apt-get install -y tesseract-ocr tesseract-ocr-kor poppler-utils
+# sudo를 모두 제거합니다.
+apt-get clean
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-kor poppler-utils
 
-# 파이썬 라이브러리 설치 (sudo 불필요)
+# 파이썬 라이브러리 설치
 pip install -r requirements.txt
