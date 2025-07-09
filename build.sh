d5d919e837bf3a1b547c168548fba6436dd38fae
이명희 <imyeonghui@imyeonghuis-MacBook-Air.local>
2025-07-09 23:36:03 +0900

    Fix apt-get update issue on Render

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
-# 1. 시스템 프로그램 설치
+# ⭐️ Read-only file system 에러 해결을 위해 폴더를 직접 생성합니다.
+mkdir -p /var/lib/apt/lists/partial
+
+# 시스템 프로그램 설치
 apt-get update
 apt-get install -y tesseract-ocr tesseract-ocr-kor poppler-utils
 
-# 2. 파이썬 라이브러리 설치
+# 파이썬 라이브러리 설치
 pip install -r requirements.txt
