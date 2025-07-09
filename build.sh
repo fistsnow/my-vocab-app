d40e1621c145f11300e3eb46b281d9efd0f73863
이명희 <imyeonghui@imyeonghuis-MacBook-Air.local>
2025-07-09 23:39:23 +0900

    Add sudo to apt-get commands

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# ⭐️ Read-only file system 에러 해결을 위해 clean을 먼저 실행
-apt-get clean
-apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-kor poppler-utils
+# ⭐️ 관리자 권한으로 실행하기 위해 sudo 추가
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr tesseract-ocr-kor poppler-utils
 
-# 파이썬 라이브러리 설치
+# 파이썬 라이브러리 설치 (sudo 불필요)
 pip install -r requirements.txt
